b9dbf2074c7b0e72a0d8276adff23bebf0606ddb
Andriy <andrijbuluk@gmail.com>
2026-02-27 21:29:46 +0100

    Actor manager Fix flake8

--- app/managers.py
+++ app/managers.py
@@ -4,20 +4,28 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __innit__(self):
+    def __init__(self) -> None:
         self._connection = sqlite3.connect("./data/actors.db")
         self.table_name = "actors"
 
+        self._connection.execute(
+            f"CREATE TABLE IF NOT EXISTS {self.table_name} ("
+            f"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            f"first_name TEXT NOT NULL, "
+            f"last_name TEXT NOT NULL"
+            f")"
 
-    def create(self, first_name: str, last_name: str):
+        )
+        self._connection.commit()
+
+    def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"INSERT INTO {self.table_name} (format) VALUES (?)",
             (first_name, last_name),
         )
         self._connection.commit()
 
-
-    def all(self):
+    def all(self) -> None:
         cursor = self._connection.execute(
             f"SELECT * FROM {self.table_name}"
         )
@@ -26,8 +34,7 @@ class ActorManager:
             Actor(*row) for row in cursor
         ]
 
-
-    def update(self, pk: str, first_name: str, last_name: str):
+    def update(self, pk: str, first_name: str, last_name: str) -> None:
         self._connection.execute(
             f"UPDATR {self.table_name} "
             f"SET {first_name}, {last_name} = ? "
@@ -36,8 +43,7 @@ class ActorManager:
         )
         self._connection.commit()
 
-
-    def delete(self, pk: str):
+    def delete(self, pk: str) -> None:
         self._connection.execute(
             f"DELETE FROM {self.table_name} "
             f"WHERE {pk} = ? ",
